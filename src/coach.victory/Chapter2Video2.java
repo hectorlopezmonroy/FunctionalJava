7a9463c507829c2065b9cc16683c8ab094bc1cdf
Hector Lopez <hectorlopezmonroy@users.noreply.github.com>
2021-09-09 09:03:42 -0500

    Fix formatting (Chapter2Video2, Chapter2Video3, NoArgFunction, and TriFunction) (#4)

--- src/coach.victory/Chapter2Video2.java
+++ src/coach.victory/Chapter2Video2.java
@@ -4,9 +4,9 @@ import java.util.function.Function;
 
 public class Chapter2Video2 {
 
-    public static void main(String[] args) {
-        Function<Integer, Integer> absoluteValue = (x) -> x < 0 ? -x : x;
+  public static void main(String[] args) {
+    Function<Integer, Integer> absoluteValue = x -> x < 0 ? -x : x;
 
-        System.out.println(absoluteValue.apply(100));
-    }
+    System.out.println(absoluteValue.apply(-100));
+  }
 }
